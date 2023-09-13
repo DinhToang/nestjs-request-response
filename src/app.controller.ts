6939dd9abdf7fc9f5f9087e944fa97d11f4a09b8
DinhToang <105490504+DinhToang@users.noreply.github.com>
2023-09-14 00:04:54 +0700

    Doi cache

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FreezePipe } from './pipes/freeze.pipe';
+import { HttpExceptionFilter } from './filters/http-eception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  //@UseFilters(HttpExceptionFilter)
   getHello(): string {
     return this.appService.getHello();
   }
+  // @Post()
+  // @UseGuards(FreezePipe)
+  // examplePost(@Body(new FreezePipe()) body: any) {
+  //   body.test = 32;
+  // }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,40 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, Scope } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RequestService } from './request.service';
+import { AuthenticationMiddleware } from './middleware/authentication.middleware';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
+import { HttpExceptionFilter } from './filters/http-eception.filter';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService],
+  providers: [
+    AppService,
+    RequestService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard, //apply to every route
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      scope: Scope.REQUEST, //injecting request scope
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthenticationMiddleware).forRoutes('*');
+    /*
+     *Register middleware and have access to it
+     * mean to apply to every route but you can change it to a specific route forRoutes.({path:"/path", method: RequestMethod.GET})
+     */
+  }
+}
